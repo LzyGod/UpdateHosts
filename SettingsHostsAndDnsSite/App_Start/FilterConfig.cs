0c222e7c2ee974897df29e28c6f4e500ef8b66b1
Administrator <Administrator@RBS116A>
2018-03-01 10:00:09 +0800

    添加项目文件。

---
+++ SettingsHostsAndDnsSite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SettingsHostsAndDnsSite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
